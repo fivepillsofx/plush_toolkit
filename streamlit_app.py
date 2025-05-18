9758fac3540eb835cb9530dedb8734968b6ba757
fivepillsofx <bradleyc4879@gmail.com>
2025-05-18 04:35:22 -0400

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Plush: The Writer Toolkit",
-    layout="wide"
-)
-
-st.title("📚 Plush: The Writer Toolkit")
-st.write("Use the menu in the top-left corner to pick a tool.")
