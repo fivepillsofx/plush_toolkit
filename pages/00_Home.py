df00348f3229a3417bbf4b5e524d7535245380de
fivepillsofx <bradleyc4879@gmail.com>
2025-05-18 04:29:19 -0400

    Delete pages directory

--- pages/00_Home.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Plush: The Writer Toolkit", layout="wide")
-st.title("📚 Plush: The Writer Toolkit")
-st.write("Welcome! Use the menu on the left to pick one of the tools.")
